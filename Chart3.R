9a2c06c0701b3301fc79bc4e91a2cc6a382b979a
Anika Surapaneni <anikas2021@gmail.com>
2023-03-03 11:09:32 -0800

    Changed the types of books to different versions of the book

--- Chart3.R
+++ Chart3.R
@@ -7,28 +7,28 @@ library(stringr)
 # Load data
 ten_checkouts <- read.csv("~/Documents/Info201code/a3-spl-checkouts-anikalsurapaneni/2017-2023-10-Checkouts-SPL-Data.csv")
 
-# Filtered data for Emma
-emma_checkouts <- ten_checkouts %>% 
-  filter(str_detect(Creator, "Jane")) %>% 
-  filter(str_detect(Creator, "Austen")) %>%
-  filter(Title == "Emma")
-
 # Filtered data for Pride and Prejudice
 pride_and_prejudice_checkouts <- ten_checkouts %>% 
   filter(str_detect(Creator, "Jane")) %>% 
   filter(str_detect(Creator, "Austen")) %>%
   filter(Title == "Pride and Prejudice")
 
-emma_and_pride_checkouts_bind <- bind_rows(emma_checkouts, pride_and_prejudice_checkouts)
+# Filtered data for Pride and Prejudice unabridged
+pride_and_prejudice_unabridged_checkouts <- ten_checkouts %>% 
+  filter(str_detect(Creator, "Jane")) %>% 
+  filter(str_detect(Creator, "Austen")) %>%
+  filter(Title == "Pride and Prejudice (unabridged)")
 
-# Stacked bar chart comparing checkouts of both books
-stacked_bar_chart_with_both_books <- ggplot(emma_and_pride_checkouts_bind, aes(x = CheckoutMonth, fill = Title)) +
+pride_and_predjudice_checkouts_bind <- bind_rows(pride_and_prejudice_checkouts, pride_and_prejudice_unabridged_checkouts)
+
+# Stacked bar chart comparing checkouts of both versions
+stacked_bar_chart_with_both_versions <- ggplot(pride_and_predjudice_checkouts_bind, aes(x = CheckoutMonth, fill = Title)) +
   geom_bar(position = "stack", stat = "count") +
   scale_x_discrete(labels = c("J", "F", "M", "A", "M", "J",
                               "J", "A", "S", "O", "N", "D")) +
-  labs(title = "Bar Chart of Emma Checkouts by Checkout Month",
+  labs(title = "Bar Chart of Pride and Prejudiced Versions",
        x = "Checkout Month",
        y = "Number of Checkouts",
        fill = "Book Title")
 
-ggplotly(stacked_bar_chart_with_both_books, tooltip = c("x", "y"))
+ggplotly(stacked_bar_chart_with_both_versions, tooltip = c("x", "y"))
